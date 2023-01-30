ca0a1c4b7e42eb8cc4015c68dc739b8bcb9e58be
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2023-01-30 12:06:39 +0600

    🛠️quote added

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -10,6 +10,11 @@ export default function Hero() {
           <br />
           OpenIslamicApp
         </h1>
+        <p>
+          Allah never said the road would be easy. But Allah said,
+          <br />
+          <q>I will be with those who are patient.</q>
+        </p>
       </div>
     </Container>
   );
@@ -20,15 +25,29 @@ const Container = styled.section`
   background-position: center;
   background-size: cover;
   aspect-ratio: 16/8;
+  @media (max-width: 390px) {
+    aspect-ratio: 4/5;
+  }
   .hero {
     display: flex;
     align-items: center;
     justify-content: center;
+    flex-direction: column;
+    gap: 24px;
     text-align: center;
     margin: auto;
     height: 100%;
-    font-size: calc(20px + 4vmin);
-    font-weight: 900;
     backdrop-filter: blur(3px);
+    & > h1 {
+      font-size: calc(20px + 4vmin);
+      font-weight: 900;
+    }
+    & > p {
+      font-size: calc(12px + 2vmin);
+      font-weight: 600;
+      & > q {
+        font-style: oblique;
+      }
+    }
   }
 `;
